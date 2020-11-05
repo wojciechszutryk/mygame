7c4c9c720927ef7c96b74049c1e79c2df7c81778
wojciechszutryk <wojtekszutryk@gmail.com>
2020-11-05 20:50:47 +0100

    building menu added to game.php

--- game.php
+++ game.php
@@ -20,6 +20,7 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="https://kit.fontawesome.com/a2f9bfa99d.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
@@ -28,14 +29,14 @@
         <h1 id="logo">MYGAME</h1>
     </header>
 
-    <section class="d-flex justify-content-center text-center mt-5">
+    <section class="d-flex justify-content-center text-center mt-5" style="width: 100%">
         <aside>
             <?php
             echo "<p> Welcome engineer ".$_SESSION['user']."! </p>";
             ?>
         </aside>
 
-        <nav>
+        <nav class="main_nav">
             <?php
             echo "<p> your email: ".$_SESSION['email'];
             echo  "<a href='logout.php'> Log out</a></p>";
@@ -45,22 +46,69 @@
 
     <div style="clear: both"></div>
 
-    <?php
-        echo " <p> Wood: ".$_SESSION['steel']." | ";
-        echo " Stone: ".$_SESSION['energy']." | ";
-        echo " Grain: ".$_SESSION['food']." | ";
+    <section class="info d-flex justify-content-center text-center mt-5" style="width: 100%">
+        <?php
+        echo ' <p> Steel: '.$_SESSION['steel'].' <i class="fas fa-tools"></i> | ';
+        echo ' Energy:'.$_SESSION['energy'].' <i class="fas fa-bolt"></i> | ';
+        echo ' Food: '.$_SESSION['food'].' <i class="fas fa-apple-alt"></i> | ';
 
         $data = new DateTime();
         $end = DateTime::createFromFormat('Y-m-d H:i:s', $_SESSION['premium']);
         $dif = $data->diff(($end));
         if ($data<$end) {
-            echo "Premium left: " . $dif->format('%d days, %h hours, %i min,  %s s');
-            echo ", until: " . $_SESSION['premium']."</p>";
+            echo "Premium left: " . $dif->format('%d days, %h hours');
         }
         else echo "No premium </p>";
+        ?>
+    </section>
 
-        echo "<p> email: ".$_SESSION['email']."</p>";
-    ?>
+    <section class="d-flex justify-content-center field_container mt-4">
+        <div class="fields">
+            <div class="field" id="field1"></div>
+            <div class="field" id="field2"></div>
+            <div class="field" id="field3"></div>
+            <div style="clear: both"></div>
+            <div class="field" id="field4"></div>
+            <div class="field" id="field5"></div>
+            <div class="field" id="field6"></div>
+            <div style="clear: both"></div>
+            <div class="field" id="field7"></div>
+            <div class="field" id="field8"></div>
+            <div class="field" id="field9"></div>
+            <div style="clear: both"></div>
+        </div>
+    </section>
+
+    <section class="action-menu">
+        <div class="d-flex justify-content-center game-menu">
+            <div class="container">
+                <ul id="menu">
+                    <a class="menu-button" href="#menu" title="Build"></a>
+                    <a class="menu-button" href="#0" title="Upgrade"></a>
+                    <li class="menu-item">
+                        <a href="#menu">
+                            <span>factory <br> <i class="fas fa-tools"></i></span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#menu">
+                            <span>platform <i class="fas fa-vector-square"></i><br> </span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#menu">
+                            <span>powerhouse <br> <i class="fas fa-bolt"></i></span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#menu">
+                            <span>farm <br> <i class="fas fa-apple-alt"></i></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
 
     <div id="comet"></div>
     <script src="scripts/comet.js"></script>
